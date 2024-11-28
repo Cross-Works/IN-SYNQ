e1dcbb48d373cb313097ca12de57ff01ac7be192
nauzijo <153818075+nauzijo@users.noreply.github.com>
2024-11-28 16:43:24 +0000

    Add files via upload
    Typo correction.

---
+++ Unreal Engine/IN-SYNQ Plugin/Source/Rhino/Public/Rhino.h
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+#include "TCPClient.h"
+#include "ProceduralMeshComponent.h"
+
+class FRhinoCommands;
+
+class FRhinoModule : public IModuleInterface
+{
+public:
+
+    /** IModuleInterface implementation */
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+    /** Polls the server for messages */
+    void PollForMessages();
+
+    /** Initializes the Procedural Mesh Component (triggered by the console command or shortcut) */
+    void InitializeMesh();
+
+private:
+    /** TCP client for communicating with the server */
+    TCPClient* MyTCPClient;
+
+    /** Procedural Mesh Component for dynamic mesh generation */
+    UProceduralMeshComponent* ProceduralMeshComponent;
+
+    /** Timer handle for polling server messages */
+    FTimerHandle PollingTimerHandle;
+};
